26148e32887fa2b94d2f96ad2fcf67a2aeab6768
manlikesach <95724242+manlikesach@users.noreply.github.com>
2025-06-06 17:11:37 +1000

    Completed step 2, added computerChoice function

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+function computerChoice() {
+  const random = Math.random();
+
+  if (random < 0.33) {
+    return "rock";
+  } else if (random < 0.66) {
+    return "paper";
+  } else {
+    return "scissor";
+  }
+}
